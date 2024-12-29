08de8b5ee07a687aa371b7852a62330be62ca36b
Pablo Rivero <pblrvo@gmail.com>
2024-12-29 18:31:56 +0100

    Bug fix

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -14,8 +13,76 @@ import (
 var _ = fmt.Fprint
 var builtinCommands = map[string]int{"echo": 0, "type": 1, "exit": 2, "pwd": 3, "cd": 4}
 
-func main() {
+func parseCommand(s string) (string, []string) {
+	// Split into command and args, preserving spaces after backslashes
+	parts := strings.SplitN(s, " ", 2)
+	command := parts[0]
 
+	if len(parts) == 1 {
+		return command, nil
+	}
+
+	argstr := parts[1]
+	var args []string
+
+	// Handle different quoting scenarios
+	var current strings.Builder
+	inDoubleQuotes := false
+	inSingleQuotes := false
+	escaped := false
+
+	for i := 0; i < len(argstr); i++ {
+		c := argstr[i]
+
+		if escaped {
+			// Handle escaped character
+			current.WriteByte(c)
+			escaped = false
+			continue
+		}
+
+		switch c {
+		case '\\':
+			if !inSingleQuotes { // Backslashes are literal in single quotes
+				escaped = true
+			} else {
+				current.WriteByte(c)
+			}
+		case '"':
+			if !inSingleQuotes {
+				inDoubleQuotes = !inDoubleQuotes
+			} else {
+				current.WriteByte(c)
+			}
+		case '\'':
+			if !inDoubleQuotes {
+				inSingleQuotes = !inSingleQuotes
+			} else {
+				current.WriteByte(c)
+			}
+		case ' ':
+			if !inDoubleQuotes && !inSingleQuotes {
+				if current.Len() > 0 {
+					args = append(args, current.String())
+					current.Reset()
+				}
+			} else {
+				current.WriteByte(c)
+			}
+		default:
+			current.WriteByte(c)
+		}
+	}
+
+	// Add the last argument if there is one
+	if current.Len() > 0 {
+		args = append(args, current.String())
+	}
+
+	return command, args
+}
+
+func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		// Wait for user input
@@ -25,36 +92,13 @@ func main() {
 		}
 
 		s = strings.Trim(s, "\r\n")
-		var args []string
-		command, argstr, _ := strings.Cut(s, " ")
-		if strings.Contains(s, "\"") {
-			re := regexp.MustCompile("\"(.*?)\"")
-			args = re.FindAllString(s, -1)
-			fmt.Println(args)
-			for i := range args {
-				args[i] = strings.ReplaceAll(args[i], "\\\"", "\"")
-				args[i] = strings.Trim(args[i], "\"")
-			}
-		} else if strings.Contains(s, "'") {
-			re := regexp.MustCompile("'(.*?)'")
-			args = re.FindAllString(s, -1)
-			for i := range args {
-				args[i] = strings.Trim(args[i], "'")
-			}
-		} else {
-			if strings.Contains(argstr, "\\") {
-				re := regexp.MustCompile(`[^\\] +`)
-				args = re.Split(argstr, -1)
-				for i := range args {
-					args[i] = strings.ReplaceAll(args[i], "\\", "")
-				}
-			} else {
-				args = strings.Fields(argstr)
-			}
+		if s == "" {
+			continue
 		}
 
-		switch command {
+		command, args := parseCommand(s)
 
+		switch command {
 		case "cd":
 			cdCommand(args)
 		case "pwd":
@@ -72,20 +116,17 @@ func main() {
 			for _, path := range paths {
 				executable := path + "/" + command
 				if _, err := os.Stat(executable); err == nil {
-					// Create the command
 					cmd := exec.Command(executable, args...)
-
-					// Set the command's standard input, output, and error streams
 					cmd.Stdin = os.Stdin
 					cmd.Stdout = os.Stdout
 					cmd.Stderr = os.Stderr
 
-					// Run the command
 					if err := cmd.Run(); err != nil {
 						fmt.Printf("Error executing file: %v\n", err)
 					} else {
 						executed = true
 					}
+					break
 				}
 			}
 
@@ -97,21 +138,26 @@ func main() {
 }
 
 func cdCommand(commands []string) {
-	if strings.TrimSpace(commands[0]) == "~" {
+	if len(commands) == 0 {
+		home := os.Getenv("HOME")
+		if err := os.Chdir(home); err != nil {
+			fmt.Fprintf(os.Stdout, "cd: %s: No such file or directory\n", home)
+		}
+		return
+	}
+
+	if commands[0] == "~" {
 		commands[0] = os.Getenv("HOME")
 	}
 
-	err := os.Chdir(commands[0])
-
-	if err != nil {
+	if err := os.Chdir(commands[0]); err != nil {
 		fmt.Fprintf(os.Stdout, "cd: %s: No such file or directory\n", commands[0])
 	}
 }
 
 func pwdCommand(commands []string) {
-	if len(commands) > 1 {
+	if len(commands) > 0 {
 		fmt.Fprint(os.Stdout, "pwd: too many arguments\n")
-		fmt.Fprint(os.Stdout, "$ ")
 		return
 	}
 
@@ -119,10 +165,14 @@ func pwdCommand(commands []string) {
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Fprint(os.Stdout, path+"\n")
+	fmt.Fprintln(os.Stdout, path)
 }
 
 func exitCommand(commands []string) {
+	if len(commands) == 0 {
+		os.Exit(0)
+	}
+
 	code, err := strconv.Atoi(commands[0])
 	if err != nil {
 		os.Exit(1)
@@ -131,11 +181,14 @@ func exitCommand(commands []string) {
 }
 
 func echoCommand(commands []string) {
-
-	fmt.Fprintf(os.Stdout, "%s\n", strings.Join(commands, " "))
+	fmt.Fprintln(os.Stdout, strings.Join(commands, " "))
 }
 
 func typeCommand(commands []string) {
+	if len(commands) == 0 {
+		return
+	}
+
 	if _, exists := builtinCommands[commands[0]]; exists {
 		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", commands[0])
 	} else {
@@ -147,6 +200,7 @@ func typeCommand(commands []string) {
 			if _, err := os.Stat(exec); err == nil {
 				fmt.Fprintf(os.Stdout, "%v is %v\n", commands[0], exec)
 				found = true
+				break
 			}
 		}
 		if !found {
